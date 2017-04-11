6bd71570dbe1a2a749ee15bf9c09f475de2fd08e
Alumno <Alumno>
2017-04-11 20:20:41 -0300

    first commit

---
+++ app/src/main/java/ar/com/developeando/tusuerte/MainActivity.java
@@ -0,0 +1,15 @@
+package ar.com.developeando.tusuerte;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Alumno on 11/4/2017.
+ */
+
+public class MainActivity extends Activity {
+    protected void onCraete(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+}
