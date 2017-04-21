95faf9090c4467df8299cb6fd3314a91b87803ea
Santiago Scalise <santi.scalise@gmail.com>
2017-04-21 19:14:12 -0300

    + salto a activity de horo chino

--- app/src/main/java/ar/com/developeando/tusuerte/MainActivity.java
+++ app/src/main/java/ar/com/developeando/tusuerte/MainActivity.java
@@ -1,7 +1,10 @@
 package ar.com.developeando.tusuerte;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Alumno on 21/4/2017.
@@ -12,5 +15,20 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Button btnCalcular = (Button) findViewById(R.id.btnCalcular);
+        Button btnChino = (Button) findViewById(R.id.btnChino);
+        btnChino.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), HoroscopoChinoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+/*        boton .setOnClickListener(new OnClickListener() {
+            protected void onClick(View c) {
+            }
+        };*/
     }
 }
