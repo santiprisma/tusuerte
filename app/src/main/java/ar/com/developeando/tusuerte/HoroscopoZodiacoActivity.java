5b23dce59af5594437bf8ec90a310761a4a19939
Santiago Scalise <santi.scalise@gmail.com>
2017-04-21 19:47:47 -0300

    validacion de numeros

--- app/src/main/java/ar/com/developeando/tusuerte/HoroscopoZodiacoActivity.java
+++ app/src/main/java/ar/com/developeando/tusuerte/HoroscopoZodiacoActivity.java
@@ -2,6 +2,9 @@ package ar.com.developeando.tusuerte;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by Alumno on 21/4/2017.
@@ -12,5 +15,29 @@ public class HoroscopoZodiacoActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_horoscopo_zodiaco);
+
+        Button btnCalcular = (Button) findViewById(R.id.btnCalcular);
+        btnCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Integer mes, año;
+
+                EditText inputMes = (EditText) findViewById(R.id.inputMes);
+                EditText inputAño = (EditText) findViewById(R.id.inputAnio);
+
+                try {
+                    mes = Integer.valueOf(inputMes.getText().toString());
+                } catch (NumberFormatException e) {
+                    inputMes.setError("El valor ingresado no es correcto.");
+                }
+
+                try {
+                    año = Integer.valueOf(inputAño.getText().toString());
+                } catch (NumberFormatException e) {
+                    inputAño.setError("El valor ingresado no es correcto.");
+                }
+            }
+        });
+
     }
 }
