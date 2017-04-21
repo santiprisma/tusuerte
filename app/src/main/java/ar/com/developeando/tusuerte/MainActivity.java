6dc4f47d55148843151b2f4d06be7754d05e8da9
Santiago Scalise <santi.scalise@gmail.com>
2017-04-21 18:59:30 -0300

    agregamos el main activity

---
+++ app/src/main/java/ar/com/developeando/tusuerte/MainActivity.java
@@ -0,0 +1,16 @@
+package ar.com.developeando.tusuerte;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Alumno on 21/4/2017.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
