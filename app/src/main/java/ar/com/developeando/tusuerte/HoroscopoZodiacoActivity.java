52f0a6ec5f9c450c2666411918187fc83262c6ca
Santiago Scalise <santi.scalise@gmail.com>
2017-04-21 20:14:33 -0300

    validacion de fechas

--- app/src/main/java/ar/com/developeando/tusuerte/HoroscopoZodiacoActivity.java
+++ app/src/main/java/ar/com/developeando/tusuerte/HoroscopoZodiacoActivity.java
@@ -5,6 +5,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import ar.com.developeando.tusuerte.model.Signo;
 
 /**
  * Created by Alumno on 21/4/2017.
@@ -20,10 +27,17 @@ public class HoroscopoZodiacoActivity extends Activity {
         btnCalcular.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Integer mes, año;
+                Integer dia = -1;
+                Integer mes = -1;
 
+                EditText inputDia = (EditText) findViewById(R.id.inputDia);
                 EditText inputMes = (EditText) findViewById(R.id.inputMes);
-                EditText inputAño = (EditText) findViewById(R.id.inputAnio);
+
+                try {
+                    dia = Integer.valueOf(inputDia.getText().toString());
+                } catch (NumberFormatException e) {
+                    inputDia.setError("El valor ingresado no es correcto.");
+                }
 
                 try {
                     mes = Integer.valueOf(inputMes.getText().toString());
@@ -31,13 +45,29 @@ public class HoroscopoZodiacoActivity extends Activity {
                     inputMes.setError("El valor ingresado no es correcto.");
                 }
 
+                // Validamos la fecha
                 try {
-                    año = Integer.valueOf(inputAño.getText().toString());
-                } catch (NumberFormatException e) {
-                    inputAño.setError("El valor ingresado no es correcto.");
+                    validateDate(dia, mes);
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), "La fecha ingresada no es correcta.", Toast.LENGTH_SHORT).show();
+                    inputDia.setText("");
+                    inputMes.setText("");
                 }
+
+             //   Signo signo = getSignoZodiaco(dia, mes);
             }
         });
-
     }
+
+    private void validateDate(Integer dia, Integer mes) throws Exception {
+        Calendar current = Calendar.getInstance();
+        Calendar c = new GregorianCalendar();
+        c.setLenient(false);
+        c.set(current.get(Calendar.YEAR), mes - 1, dia);
+        c.getTime();
+    }
+
+    /*private Signo getSignoZodiaco(Integer dia, Integer mes) {
+        if ((month == 12 && day >= 22 && day <= 31) || (month ==  1 && day >= 1 && day <= 19))
+    }*/
 }
