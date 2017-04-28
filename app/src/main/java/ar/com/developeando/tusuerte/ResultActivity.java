1d711ef22922961a34ce83e67a9f322d2df00f3a
Alumno <Alumno>
2017-04-28 20:53:14 -0300

    acciones email / SMS

--- app/src/main/java/ar/com/developeando/tusuerte/ResultActivity.java
+++ app/src/main/java/ar/com/developeando/tusuerte/ResultActivity.java
@@ -1,6 +1,8 @@
 package ar.com.developeando.tusuerte;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -17,13 +19,16 @@ import ar.com.developeando.tusuerte.model.Signo;
  */
 
 public class ResultActivity extends AppCompatActivity {
+
+    Signo signo = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado);
 
         // Obtenemos el intent
-        Signo signo = (Signo) getIntent().getSerializableExtra("signo");
+        signo = (Signo) getIntent().getSerializableExtra("signo");
 
         // Ponemos el nombre del signo
         TextView textSigno = (TextView) findViewById(R.id.textSigno);
@@ -47,11 +52,27 @@ public class ResultActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent = null;
+
         switch (item.getItemId()) {
             case R.id.itemEmail:
+                intent = new Intent(Intent.ACTION_SENDTO);
+                intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+                intent.putExtra(Intent.EXTRA_EMAIL, "");
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Mi horóscopo");
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
                 return true;
 
             case R.id.itemSMS:
+                intent = new Intent(Intent.ACTION_SEND);
+                intent.setData(Uri.parse("smsto:"));  // This ensures only SMS apps respond
+                intent.putExtra("sms_body", "Mi signo es: "+ signo.getNombre());
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+
                 return true;
 
             case R.id.itemCompartir:
